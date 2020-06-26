56d48bf30ba6b20cd96fe8d5a1b070d4111c8620
pvorobey <Vorobey_Pavel@mail.ru>
2020-06-26 20:16:49 +0300

    remote work

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,6 @@
 public class MyFirstProgram	{
 	
-	public static void main (String[] chtou) {
+	public static void main (String[] chtou!!!!!!) {
 		for(String name : chtou){
 
 		System.out.println("Hello !!!!!" + name);
