8ea7c233ca65f72d99fe2a314ae02d3978dd481f
pvorobey <Vorobey_Pavel@mail.ru>
2020-06-26 19:39:08 +0300

    second commit

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -3,7 +3,7 @@ public class MyFirstProgram	{
 	public static void main (String[] chtou) {
 		for(String name : chtou){
 
-		System.out.println("Hello " + name);
+		System.out.println("Hello !!!!!" + name);
 		}
 	}
 }
